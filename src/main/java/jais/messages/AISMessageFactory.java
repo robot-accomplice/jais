4e7f41a8185bfb9f78c3d356c638fd7ba5642513
Jonathan Machen <jonathan.machen@robotaccomplice.com>
2018-02-26 16:07:38 +0100

    replaced reflection based AISMessage creation with a switch statement

--- src/main/java/jais/messages/AISMessageFactory.java
+++ src/main/java/jais/messages/AISMessageFactory.java
@@ -101,44 +101,105 @@ public class AISMessageFactory {
             compositeMsg = AISPacket.bArray2Str( compositeBytes );
             
             if( LOG.isDebugEnabled() ) LOG.debug( "Composite message is: {}", compositeMsg );
+            
             // we need the message type in order to invoke the reflective constructor
-            AISMessageType mType = AISMessageDecoder.decodeMessageType( compositeMsg );
+            Optional<AISMessageType> mType = AISMessageDecoder.decodeMessageType( compositeMsg );
 
-            if( mType != null ) {
-                if( LOG.isDebugEnabled() ) LOG.debug( "Creating a new {} instance.", mType.getDescription() );
+            if( mType.isPresent() ) {
+                if( LOG.isDebugEnabled() ) LOG.debug( "Creating a new {} instance.", mType.get().getDescription() );
 
-                Constructor con = mType.getMessageClass().getDeclaredConstructor( String.class, AISPacket[].class );
-                con.setAccessible( true );
-
-                AISMessage message;
-                if( packets.length == 1 ) {
-                    // reflection won't work properly for singletons if we don't do this
-                    message = ( AISMessage ) con.newInstance( ( Object )source, ( Object )packets );
-                } else {
-                    message = ( AISMessage ) con.newInstance( source, packets );
+                AISMessage message = null;
+                switch( mType.get() ) {
+                    case POSITION_REPORT_CLASS_A:
+                        message = new PositionReportClassA( source, packets );
+                        break;
+                    case POSITION_REPORT_CLASS_A_ASSIGNED_SCHEDULE:
+                        message = new PositionReportClassAAssignedSchedule( source, packets );
+                        break;
+                    case POSITION_REPORT_CLASS_A_RESPONSE_TO_INTERROGATION:
+                        message = new PositionReportClassAResponseToInterrogation( source, packets );
+                        break;
+                    case BASE_STATION_REPORT:
+                        message = new BaseStationReport( source, packets );
+                        break;
+                    case STATIC_AND_VOYAGE_RELATED_DATA:
+                        message = new StaticAndVoyageRelatedData( source, packets );
+                        break;
+                    case BINARY_ACKNOWLEDGE:
+                        message = new BinaryAcknowledge( source, packets );
+                        break;
+                    case STANDARD_SAR_AIRCRAFT_POSITION_REPORT:
+                        message = new StandardSARAircraftPositionReport( source, packets );
+                        break;
+                    case UTC_AND_DATE_INQUIRY:
+                        message = new UTCDateInquiry( source, packets );
+                        break;
+                    case UTC_AND_DATE_RESPONSE:
+                        message = new UTCDateResponse( source, packets );
+                        break;
+                    case ADDRESSED_SAFETY_RELATED_MESSAGE:
+                        message = new AddressedSafetyRelatedMessage( source, packets );
+                        break;
+                    case SAFETY_RELATED_ACKNOWLEDGEMENT:
+                        message = new SafetyRelatedAcknowledgement( source, packets );
+                        break;
+                    case SAFETY_RELATED_BROADCAST_MESSAGE:
+                        message = new SafetyRelatedBroadcastMessage( source, packets );
+                        break;
+                    case INTERROGATION:
+                        message = new Interrogation( source, packets );
+                        break;
+                    case ASSIGNMENT_MODE_COMMAND:
+                        message = new AssignmentModeCommand( source, packets );
+                        break;
+                    case DGNSS_BROADCAST_BINARY_MESSAGE:
+                        message = new DGNSSBroadcastBinaryMessage( source, packets );
+                        break;
+                    case STANDARD_CLASS_B_CS_POSITION_REPORT:
+                        message = new StandardClassBCSPositionReport( source, packets );
+                        break;
+                    case EXTENDED_CLASS_B_CS_POSITION_REPORT:
+                        message = new ExtendedClassBCSPositionReport( source, packets );
+                        break;
+                    case DATA_LINK_MANAGEMENT_MESSAGE:
+                        message = new DataLinkManagementMessage( source, packets );
+                        break;
+                    case AID_TO_NAVIGATION_REPORT:
+                        message = new AidToNavigationReport( source, packets );
+                        break;
+                    case CHANNEL_MANAGEMENT:
+                        message = new ChannelManagement( source, packets );
+                        break;
+                    case GROUP_ASSIGNMENT_COMMAND:
+                        message = new GroupAssignmentCommand( source, packets );
+                        break;
+                    case STATIC_DATA_REPORT:
+                        message = new StaticDataReport( source, packets );
+                        break;
+                    case SINGLE_SLOT_BINARY_MESSAGE:
+                        message = new SingleSlotBinaryMessage( source, packets );
+                        break;
+                    case MULTIPLE_SLOT_BINARY_MESSAGE:
+                        message = new MultipleSlotBinaryMessage( source, packets );
+                        break;
+                    case POSITION_REPORT_FOR_LONG_RANGE_APPLICATIONS:
+                        message = new LongRangeAISBroadcastMessage( source, packets );
+                        break;
+                    default:
+                        LOG.warn( "{} - Unknown or invalid message type ", source );
+                        return Optional.empty();
                 }
 
-                message.setType( mType );
-                if( source != null ) {
-                    message.setSource( source );
-                } else if( packets[0].getSource() != null ) {
-                    message.setSource( AISPacket.bArray2Str( packets[0].getSource(), charset ) );
-                } else {
-                    message.setSource( DEFAULT_SOURCE );
+                if( message != null ) {
+                    // decode message
+                    message.decode( charset );
                 }
-                while( message.hasSubType() ) {
-                    message = message.getSubTypeInstance( charset );
-                }
-
-                // decode message
-                message.decode( charset );
+                
                 return Optional.of( message );
             } else {
                 throw new AISException( "MessageType is null for message String: " + compositeMsg );
             }
-        } catch( AISException | NoSuchMethodException | SecurityException |
-                InstantiationException | IllegalAccessException |
-                IllegalArgumentException | InvocationTargetException t ) {
+        } catch( AISException t ) {
             // repackage any and all throwables as AISExceptions
             if( strict ) {
                 throw new AISException( "Failed to create a valid AISMessage from packet \"" + compositeMsg + "\" : " + t.getMessage(), t );
