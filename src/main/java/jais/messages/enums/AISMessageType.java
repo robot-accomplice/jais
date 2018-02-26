6c96077536f3d3a7d8e755e0908823284e5b34c8
Jonathan Machen <jonathan.machen@robotaccomplice.com>
2018-02-26 16:19:45 +0100

    removed references to Java classes for message types

--- src/main/java/jais/messages/enums/AISMessageType.java
+++ src/main/java/jais/messages/enums/AISMessageType.java
@@ -16,34 +16,6 @@
 
 package jais.messages.enums;
 
-import jais.messages.MultipleSlotBinaryMessage;
-import jais.messages.UTCDateResponse;
-import jais.messages.BinaryAcknowledge;
-import jais.messages.DataLinkManagementMessage;
-import jais.messages.StandardSARAircraftPositionReport;
-import jais.messages.GroupAssignmentCommand;
-import jais.messages.SingleSlotBinaryMessage;
-import jais.messages.PositionReportClassAAssignedSchedule;
-import jais.messages.SafetyRelatedBroadcastMessage;
-import jais.messages.StaticDataReport;
-import jais.messages.BinaryBroadcastMessageBase;
-import jais.messages.Interrogation;
-import jais.messages.StandardClassBCSPositionReport;
-import jais.messages.ChannelManagement;
-import jais.messages.PositionReportClassAResponseToInterrogation;
-import jais.messages.PositionReportClassA;
-import jais.messages.SafetyRelatedAcknowledgement;
-import jais.messages.AddressedSafetyRelatedMessage;
-import jais.messages.LongRangeAISBroadcastMessage;
-import jais.messages.AidToNavigationReport;
-import jais.messages.BaseStationReport;
-import jais.messages.AssignmentModeCommand;
-import jais.messages.UTCDateInquiry;
-import jais.messages.DGNSSBroadcastBinaryMessage;
-import jais.messages.StaticAndVoyageRelatedData;
-import jais.messages.ExtendedClassBCSPositionReport;
-import jais.messages.BinaryAddressedMessageBase;
-
 /**
  *
  * @author Jonathan Machen
@@ -52,116 +24,59 @@ public enum AISMessageType {
 
     // these are sent every 2 to 10 seconds while underway (depending on speed)
     // and every 3 minutes while at anchor and stationary
-    POSITION_REPORT_CLASS_A( 1, 
-            "Position Report Class A", 
-            PositionReportClassA.class ),
-    POSITION_REPORT_CLASS_A_ASSIGNED_SCHEDULE( 2, 
-            "Position Report Class A (Assigned Schedule)", 
-            PositionReportClassAAssignedSchedule.class ),
-    POSITION_REPORT_CLASS_A_RESPONSE_TO_INTERROGATION( 3, 
-            "Position Report Class A (Response to interrogation)",
-            PositionReportClassAResponseToInterrogation.class ),
-    
-    BASE_STATION_REPORT( 4, 
-            "Base Station Report", 
-            BaseStationReport.class ),
-    
+    POSITION_REPORT_CLASS_A( 1, "Position Report Class A" ),
+    POSITION_REPORT_CLASS_A_ASSIGNED_SCHEDULE( 2, "Position Report Class A (Assigned Schedule)" ),
+    POSITION_REPORT_CLASS_A_RESPONSE_TO_INTERROGATION( 3, "Position Report Class A (Response to interrogation)" ),
+    BASE_STATION_REPORT( 4, "Base Station Report" ),
     // sent every 6 minutes
-    STATIC_AND_VOYAGE_RELATED_DATA( 5, 
-            "Static & Voyage Related Data", 
-            StaticAndVoyageRelatedData.class),
+    STATIC_AND_VOYAGE_RELATED_DATA( 5, "Static & Voyage Related Data" ),
     
     // local authority 
-    BINARY_ADDRESSED_MESSAGE( 6, 
-            "Binary Addressed Message", 
-            BinaryAddressedMessageBase.class ),
+    BINARY_ADDRESSED_MESSAGE( 6, "Binary Addressed Message" ),
     
-    BINARY_ACKNOWLEDGE( 7, 
-            "Binary Acknowledge", 
-            BinaryAcknowledge.class ),
+    BINARY_ACKNOWLEDGE( 7, "Binary Acknowledge" ),
     
     // encrypted private message, usually military
-    BINARY_BROADCAST_MESSAGE( 8, 
-            "Binary Broadcast Message", 
-            BinaryBroadcastMessageBase.class ),
-    STANDARD_SAR_AIRCRAFT_POSITION_REPORT( 9, 
-            "Standard SAR Aircraft Position Report", 
-            StandardSARAircraftPositionReport.class ),
-    UTC_AND_DATE_INQUIRY( 10, 
-            "UTC & Date Inquiry", 
-            UTCDateInquiry.class ),
-    UTC_AND_DATE_RESPONSE( 11, 
-            "UTC & Date Response", 
-            UTCDateResponse.class ),
+    BINARY_BROADCAST_MESSAGE( 8, "Binary Broadcast Message" ),
+    STANDARD_SAR_AIRCRAFT_POSITION_REPORT( 9, "Standard SAR Aircraft Position Report" ),
+    UTC_AND_DATE_INQUIRY( 10, "UTC & Date Inquiry" ),
+    UTC_AND_DATE_RESPONSE( 11, "UTC & Date Response" ),
     
     // text messaging, safety-related, traffic control, chatter
-    ADDRESSED_SAFETY_RELATED_MESSAGE( 12, 
-            "Addressed Safety Related Message", 
-            AddressedSafetyRelatedMessage.class ),
+    ADDRESSED_SAFETY_RELATED_MESSAGE( 12, "Addressed Safety Related Message" ),
     
-    SAFETY_RELATED_ACKNOWLEDGEMENT( 13, 
-            "Safety Related Acknowledgement", 
-            SafetyRelatedAcknowledgement.class ),
+    SAFETY_RELATED_ACKNOWLEDGEMENT( 13, "Safety Related Acknowledgement" ),
 
     // text messaging, safety-related, traffic control, chatter
-    SAFETY_RELATED_BROADCAST_MESSAGE( 14, 
-            "Safety Related Broadcast Message", 
-            SafetyRelatedBroadcastMessage.class ),
+    SAFETY_RELATED_BROADCAST_MESSAGE( 14, "Safety Related Broadcast Message" ),
     
-    INTERROGATION( 15, 
-            "Interrogation", 
-            Interrogation.class ),
-    ASSIGNMENT_MODE_COMMAND( 16, 
-            "Assignment Mode Command", 
-            AssignmentModeCommand.class ),
-    DGNSS_BROADCAST_BINARY_MESSAGE( 17, 
-            "DGNSS Broadcast Binary Message", 
-            DGNSSBroadcastBinaryMessage.class ),
-    STANDARD_CLASS_B_CS_POSITION_REPORT( 18, 
-            "Standard Class B CS Position Report", 
-            StandardClassBCSPositionReport.class ),
-    EXTENDED_CLASS_B_CS_POSITION_REPORT( 19, 
-            "Extended Class B Equipment Position Report", 
-            ExtendedClassBCSPositionReport.class ),
-    DATA_LINK_MANAGEMENT_MESSAGE( 20, 
-            "Datalink Management Message", 
-            DataLinkManagementMessage.class ),
-    AID_TO_NAVIGATION_REPORT( 21, 
-            "Aid-to-Navigation Report", 
-            AidToNavigationReport.class ),
-    CHANNEL_MANAGEMENT( 22, 
-            "Channel Management", 
-            ChannelManagement.class ),
-    GROUP_ASSIGNMENT_COMMAND( 23, 
-            "Group Assignment Command", 
-            GroupAssignmentCommand.class ),
-    STATIC_DATA_REPORT( 24, 
-            "Static Data Report", 
-            StaticDataReport.class ),
-    SINGLE_SLOT_BINARY_MESSAGE( 25, 
-            "Single Slot Binary Message", 
-            SingleSlotBinaryMessage.class ),
-    MULTIPLE_SLOT_BINARY_MESSAGE( 26, 
-            "Multiple Slot Binary Message With Communications State", 
-            MultipleSlotBinaryMessage.class ),
-    POSITION_REPORT_FOR_LONG_RANGE_APPLICATIONS( 27, 
-            "Position Report For Long Range Applications", 
-            LongRangeAISBroadcastMessage.class );
+    INTERROGATION( 15, "Interrogation" ),
+    ASSIGNMENT_MODE_COMMAND( 16, "Assignment Mode Command" ),
+    DGNSS_BROADCAST_BINARY_MESSAGE( 17, "DGNSS Broadcast Binary Message" ),
+    STANDARD_CLASS_B_CS_POSITION_REPORT( 18, "Standard Class B CS Position Report" ),
+    EXTENDED_CLASS_B_CS_POSITION_REPORT( 19, "Extended Class B Equipment Position Report" ),
+    DATA_LINK_MANAGEMENT_MESSAGE( 20, "Datalink Management Message" ),
+    AID_TO_NAVIGATION_REPORT( 21, "Aid-to-Navigation Report" ), 
+    CHANNEL_MANAGEMENT( 22, "Channel Management" ), 
+    GROUP_ASSIGNMENT_COMMAND( 23, "Group Assignment Command" ), 
+    STATIC_DATA_REPORT( 24, "Static Data Report" ), 
+    SINGLE_SLOT_BINARY_MESSAGE( 25, "Single Slot Binary Message" ), 
+    MULTIPLE_SLOT_BINARY_MESSAGE( 26, "Multiple Slot Binary Message With Communications State" ), 
+    POSITION_REPORT_FOR_LONG_RANGE_APPLICATIONS( 27, "Position Report For Long Range Applications" );
+    
     /*
      * private variables 
      */
     private final int _id;
     private final String _description;
-    private final Class _messageClass;
     
     /**
      * 
      * @param description 
      */
-    AISMessageType( int id, String description, Class messageClass ) {
+    AISMessageType( int id, String description ) {
         _id = id;
         _description = description;
-        _messageClass = messageClass;
     }
     
     /**
@@ -182,14 +97,6 @@ public enum AISMessageType {
     
     /**
      * 
-     * @return 
-     */
-    public Class<?> getMessageClass() {
-        return _messageClass;
-    }
-    
-    /**
-     * 
      * @param id 
      * @return  
      */
@@ -198,24 +105,6 @@ public enum AISMessageType {
         
         for( AISMessageType type : AISMessageType.values() ) {
             if( type.getId() == id ) {
-                foundType = type;
-                break;
-            }
-        }
-        
-        return foundType;
-    }
-    
-    /**
-     * 
-     * @param messageClass
-     * @return 
-     */
-    public static AISMessageType fetchByMessageClass( Class messageClass ) {
-        AISMessageType foundType = null;
-        
-        for( AISMessageType type : AISMessageType.values() ) {
-            if( type.getMessageClass().equals( messageClass ) ) {
                 foundType = type;
                 break;
             }
