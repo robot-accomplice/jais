97327fe508970eb0db52ed18ace9f987543f0076
Jonathan Machen <jonathan.machen@robotaccomplice.com>
2023-08-03 11:12:21 +0200

    add toString method with human-readable text

--- src/main/java/jais/messages/enums/VesselClass.java
+++ src/main/java/jais/messages/enums/VesselClass.java
@@ -4,10 +4,17 @@ package jais.messages.enums;
  * Distinguishes between class A and class B vessels in a type safe way
  */
 public enum VesselClass {
-    UNSPECIFIED,
-    A,
-    B;
+    UNSPECIFIED("Class Unspecified"),
+    A("Class A"),
+    B("Class B");
 
-    VesselClass() {
+    private final String nameStr;
+
+    VesselClass(String nameStr) {
+        this.nameStr = nameStr;
+    }
+
+    public String toString() {
+        return this.nameStr;
     }
 }
